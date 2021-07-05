2cc13645bdb6936e60394c5c724f160715fdb806
Matthew Smith <matt@beamend.com>
2021-07-05 17:49:49 +0100

    updated

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,165 +1,174 @@
-import { FormHelperText } from '@material-ui/core';
-import React, { Component } from 'react';
-import { Grid, Button, Typography } from '@material-ui/core';
+import { FormHelperText } from "@material-ui/core";
+import React, { Component } from "react";
+import { Grid, Button, Typography } from "@material-ui/core";
 import CreateRoomPage from "./CreateRoomPage";
-
 
 // Creating the Room page component, constructor defines the default values
 export default class Room extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            votesToSkip: 2,
-            guestCanPause: false,
-            isHost: false,
-            showSettings: false,
-            spotifyAuthenticated: false,
-        };
-        this.roomCode = this.props.match.params.roomCode;
-        this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
-        this.updateShowSettings = this.updateShowSettings.bind(this);
-        this.renderSettingsButton = this.renderSettingsButton.bind(this);
-        this.renderSettings = this.renderSettings.bind(this);
-        this.getRoomDetails = this.getRoomDetails.bind(this);
-        this.authenticateSpotify = this.authenticateSpotify.bind(this);
-        this.getRoomDetails();
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      votesToSkip: 2,
+      guestCanPause: false,
+      isHost: false,
+      showSettings: false,
+      spotifyAuthenticated: false,
+    };
+    this.roomCode = this.props.match.params.roomCode;
+    this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
+    this.updateShowSettings = this.updateShowSettings.bind(this);
+    this.renderSettingsButton = this.renderSettingsButton.bind(this);
+    this.renderSettings = this.renderSettings.bind(this);
+    this.getRoomDetails = this.getRoomDetails.bind(this);
+    this.authenticateSpotify = this.authenticateSpotify.bind(this);
+    this.getRoomDetails();
+  }
 
-    // Request the Room details from getRoom view object using the room code specified.
-    getRoomDetails() {
-        return fetch("/api/get-room" + "?code=" + this.roomCode)
-          .then((response) => {
-            if (!response.ok) {
-              this.props.leaveRoomCallback();
-              this.props.history.push("/");
-            }
-            return response.json();
-          })
-          .then((data) => {
-            this.setState({
-              votesToSkip: data.votes_to_skip,
-              guestCanPause: data.guest_can_pause,
-              isHost: data.is_host,
-            });
-            if (this.state.isHost) {
-                this.authenticateSpotify();
-            }
-          });
-    }
-
-    // Send request to Spotify API backend asking if the user is authenticated.
-    // But only when the user is a host.
-    authenticateSpotify() {
-        fetch("/spotify/is-authenticated")
-          .then((response) => response.json())
-          .then((data) => {
-            this.setState({ spotifyAuthenticated: data.status });
-            console.log(data.status);
-            if (!data.status) {
-              fetch("/spotify/get-auth-url")
-                .then((response) => response.json())
-                .then((data) => {
-                  window.location.replace(data.url);
-                });
-            }
-          });
-    }
-
-    // When leave room button is pressed send a request to backend to remove room code
-    // from users session, and check if users leaving is the host in which case delete 
-    // the room from database.Then redirect user back to the home page.
-    leaveButtonPressed() {
-        const requestOptions = {
-            method: "POST",
-            headers: {'Content-Type': "applications/json"},
-        };
-        fetch("/api/leave-room", requestOptions).then((_response) => {
-            this.props.leaveRoomCallback();
-            this.props.history.push("/");
-        });
-    }
-
-    // Used to handle whether the settings must be shown or not
-    // This will depend on whether the settings button has been clicked
-    updateShowSettings(value) {
-        this.setState({
-            showSettings: value,
-        });
-    }
-
-    // The build code for the room settings page
-    // Import and use the CreateRoomPage component as the template for updating a rooms
-    renderSettings() {
-        return (
-            <Grid container spacing={1}>
-                <Grid item xs={12} align="center">
-                    <CreateRoomPage
-                        update={true}
-                        votesToSkip={this.state.votesToSkip}
-                        guestCanPause={this.state.guestCanPause}
-                        roomCode={this.roomCode}
-                        updateCallback={this.getRoomDetails}
-                    />
-                </Grid>
-                <Grid item xs={12} align="center">
-                    <Button variant="contained" color="secondary" onClick={() => this.updateShowSettings(false)}>
-                        Close
-                    </Button>
-                </Grid>
-            </Grid>
-        ) 
-    }
-
-
-    // Render the settings button seperate to the main build code
-    // This allows us to enable/disable the user from accessing the settings
-    // depending on whether their a host or not.
-    renderSettingsButton() {
-        return (
-            <Grid item xs={12} align="center">
-                <Button variant="contained" color="primary" onClick={() => this.updateShowSettings(true)}>
-                    Settings
-                </Button>
-            </Grid>
-        );
-    }
-
-
-    // The build code for the Room page.
-    // If showSettings is true then render settings page instead of room page.
-    render() {
-        if (this.state.showSettings) {
-            return this.renderSettings()
+  // Request the Room details from getRoom view object using the room code specified.
+  getRoomDetails() {
+    return fetch("/api/get-room" + "?code=" + this.roomCode)
+      .then((response) => {
+        if (!response.ok) {
+          this.props.leaveRoomCallback();
+          this.props.history.push("/");
         }
-        return (
-            <Grid container spacing={1}>
-                <Grid item xs={12} align='center'>
-                    <Typography variant="h4" component="h4">
-                        Code: {this.roomCode}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align='center'>
-                    <Typography variant="h6" component="h6">
-                        Votes: {this.state.votesToSkip}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align='center'>
-                    <Typography variant="h6" component="h6">
-                        Guest can Pause: {this.state.guestCanPause.toString()}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} align='center'>
-                    <Typography variant="h6" component="h6">
-                        Host: {this.state.isHost.toString()}
-                    </Typography>
-                </Grid>
-                {this.state.isHost ? this.renderSettingsButton() : null}
-                <Grid item xs={12} align='center'>
-                    <Button variant="contained" color="secondary" onClick={ this.leaveButtonPressed }>
-                        Leave Room
-                    </Button>
-                </Grid>
-            </Grid>         
-        )   
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          votesToSkip: data.votes_to_skip,
+          guestCanPause: data.guest_can_pause,
+          isHost: data.is_host,
+        });
+        if (this.state.isHost) {
+          this.authenticateSpotify();
+        }
+      });
+  }
+
+  // Send request to Spotify API backend asking if the user is authenticated.
+  // But only when the user is a host.
+  authenticateSpotify() {
+    fetch("/spotify/is-authenticated")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ spotifyAuthenticated: data.status });
+        console.log(data.status);
+        if (!data.status) {
+          fetch("/spotify/get-auth-url")
+            .then((response) => response.json())
+            .then((data) => {
+              window.location.replace(data.url);
+            });
+        }
+      });
+  }
+
+  // When leave room button is pressed send a request to backend to remove room code
+  // from users session, and check if users leaving is the host in which case delete
+  // the room from database.Then redirect user back to the home page.
+  leaveButtonPressed() {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "applications/json" },
+    };
+    fetch("/api/leave-room", requestOptions).then((_response) => {
+      this.props.leaveRoomCallback();
+      this.props.history.push("/");
+    });
+  }
+
+  // Used to handle whether the settings must be shown or not
+  // This will depend on whether the settings button has been clicked
+  updateShowSettings(value) {
+    this.setState({
+      showSettings: value,
+    });
+  }
+
+  // The build code for the room settings page
+  // Import and use the CreateRoomPage component as the template for updating a rooms
+  renderSettings() {
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <CreateRoomPage
+            update={true}
+            votesToSkip={this.state.votesToSkip}
+            guestCanPause={this.state.guestCanPause}
+            roomCode={this.roomCode}
+            updateCallback={this.getRoomDetails}
+          />
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => this.updateShowSettings(false)}
+          >
+            Close
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  // Render the settings button seperate to the main build code
+  // This allows us to enable/disable the user from accessing the settings
+  // depending on whether their a host or not.
+  renderSettingsButton() {
+    return (
+      <Grid item xs={12} align="center">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => this.updateShowSettings(true)}
+        >
+          Settings
+        </Button>
+      </Grid>
+    );
+  }
+
+  // The build code for the Room page.
+  // If showSettings is true then render settings page instead of room page.
+  render() {
+    if (this.state.showSettings) {
+      return this.renderSettings();
     }
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h4" component="h4">
+            Code: {this.roomCode}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Votes: {this.state.votesToSkip}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Guest can Pause: {this.state.guestCanPause.toString()}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <Typography variant="h6" component="h6">
+            Host: {this.state.isHost.toString()}
+          </Typography>
+        </Grid>
+        {this.state.isHost ? this.renderSettingsButton() : null}
+        <Grid item xs={12} align="center">
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={this.leaveButtonPressed}
+          >
+            Leave Room
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
 }
--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -4,7 +4,8 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import is_spotify_authenticated, update_or_create_user_tokens, is_spotify_authenticated
+from .util import *
+from api.models import Room
 
 # View to allow requests for authorization to access data from Spotify
 class AuthURL(APIView):
@@ -54,3 +55,18 @@ class IsAuthenticated(APIView):
     def get(self, request, format=None):
         is_authenticated = is_spotify_authenticated(self.request.session.session_key)
         return Response({'status': is_authenticated}, status=status.HTTP_200_OK)
+
+
+class CurrentSong(APIView):
+    def get(self, request, format=None):
+        room_code = self.request.session.get('room_code')
+        room = Room.objects.filter(code=room_code)
+        if room.exists():
+            room  = room[0]
+        else:
+            return Reponse({}, status=status.HTTP_404_NOT_FOUND)
+        host = room.host
+        endpoint = "/player/currently-playing"
+        response = execute_spotify_api_request(host, endpoint)
+        print(response)
+        return Response(response, status=status.HTTP_200_OK)
